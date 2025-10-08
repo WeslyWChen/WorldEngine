8aea815529b48b38a07fc96704301ad6eaf338d5
chenwei <306325111@qq.com>
2025-10-08 17:24:48 +0800

    feat:增加消息中心模块

---
+++ Engine/SignalCenter/Private/SignalCenterInterface.cpp
@@ -0,0 +1,20 @@
+//
+// Created by 30632 on 2025/10/8.
+//
+
+#include "SignalCenterInterface.h"
+
+#include "SignalCenter.h"
+
+using namespace std;
+using namespace WorldEngine;
+
+std::shared_ptr<ISignalCenter> WorldEngine::CreateSignalCenter()
+{
+    return make_shared<SignalCenter>();
+}
+
+ISignalCenter& WorldEngine::GetSignalCenter()
+{
+    return *SignalCenter::Instance();
+}
---
+++ Test/TestSignalCenter.cpp
@@ -0,0 +1,53 @@
+//
+// Created by 30632 on 2025/10/8.
+//
+
+#include <chrono>
+
+#include "gtest/gtest.h"
+#include "ModuleInterface.h"
+#include "SignalCenterInterface.h"
+
+using namespace std;
+using namespace WorldEngine;
+
+TEST(TestSignalCenter, Base)
+{
+    auto signalCenter = CreateSignalCenter();
+
+    int count = 0;
+    signalCenter->connect("test", [&count] { count++; });
+    signalCenter->emitSignal("test");
+    ASSERT_EQ(count, 1);
+
+    signalCenter->emitSignal("test");
+    ASSERT_EQ(count, 2);
+}
+
+TEST(TestSignalCenter, MainThreadRun)
+{
+    auto signalCenter = CreateSignalCenter();
+
+    int count = 0;
+    signalCenter->connect("test", [&count] { count++; });
+    signalCenter->emitSignal("test", SignalTrigger::MAIN_THREAD);
+    ASSERT_EQ(count, 0);
+    signalCenter->runMainThread();
+    ASSERT_EQ(count, 1);
+}
+
+TEST(TestSignalCenter, SignalThreadRun)
+{
+    using namespace std::chrono;
+
+    auto signalCenter = CreateSignalCenter();
+    signalCenter->startupSignalThread();
+
+    int count = 0;
+    signalCenter->connect("test", [&count] { count++; });
+    signalCenter->emitSignal("test", SignalTrigger::SIGNAL_THREAD);
+    ASSERT_TRUE(count == 0 || count == 1);
+    this_thread::sleep_for(500ms);
+    signalCenter->shutdownSignalThread();
+    EXPECT_EQ(count, 1);
+}
